a1e5cee1d9150f2688a1f398270c99011951aee4
kun-all24 <kuunnal71@gmail.com>
2024-12-22 11:32:41 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import ProductList from "./components/ProductList";
-// import Cart from "./components/Cart";
-// import ProductDetail from "./components/ProductDetail"; // Import ProductDetail component
-// import NotFound from "./components/NotFound";
-
-// const App = () => (
-//   <Router>
-//     <div className="min-h-screen bg-gray-100">
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<ProductList />} />
-//         <Route path="/cart" element={<Cart />} />
-//         <Route path="/products/:id" element={<ProductDetail />} /> {/* Add route for product details */}
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </div>
-//   </Router>
-// );
-
-// export default App;
-
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";

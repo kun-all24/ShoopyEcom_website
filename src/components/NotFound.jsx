1307bd393d8eb68f52c1ba2b1ecf26d8265d95a0
kun-all24 <kuunnal71@gmail.com>
2024-12-29 16:53:09 +0530

    intial changes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
     <div className="flex items-center justify-center min-h-screen bg-sky-100">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-sky-400 mb-4">404</h1>
-        <p className="text-gray-700 text-lg mb-6">The page you are looking for cannot be found.</p>
+        <p className="text-gray-700 text-lg  mb-6">The page you are looking for cannot be found.</p>
         <a
           href="/"
           className="bg-sky-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-slate-600 transition"

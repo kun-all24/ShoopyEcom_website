42146a027d8652b7e7143a950725e69091cdf51a
kun-all24 <kuunnal71@gmail.com>
2025-01-04 09:35:47 +0530

    intial state

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-sky-100">
+    <div className="flex  items-center justify-center min-h-screen bg-sky-100">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-sky-400 mb-4">404</h1>
         <p className="text-gray-700 text-lg  mb-6">The page you are looking for cannot be found.</p>
